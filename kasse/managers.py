2146857c929219f401da2941d6cd9b13d82a6696
Mathias Rav <rav@cs.au.dk>
2015-09-03 12:26:48 +0200

    Add RawSQL code to compute prefix sums

--- kasse/managers.py
+++ kasse/managers.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals, division
 
 from django.db import models
 from django.db.models import Count, Sum
+from django.db.models.expressions import RawSQL
 
 
 class ProfileManager(models.Manager):
@@ -21,3 +22,16 @@ class TimeTrialManager(models.Manager):
         qs = qs.annotate(leg_count=Count('leg'))
         qs = qs.annotate(duration=Sum('leg__duration'))
         return qs.select_related('profile', 'creator')
+
+
+class LegManagerPrototype(models.Manager):
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        qs = super(LegManagerPrototype, self).get_queryset()
+        sql = '''
+            SELECT SUM(duration) FROM kasse_leg `a`
+            WHERE a.timetrial_id = kasse_leg.timetrial_id
+            AND a.order <= kasse_leg.order'''
+        qs = qs.annotate(p=RawSQL(sql, ()))
+        return qs
