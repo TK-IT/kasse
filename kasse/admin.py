32f714ebf7423be56799450d7a9a19d97522ed52
Mathias Rav <rav@cs.au.dk>
2015-09-19 14:26:55 +0200

    Add some ModelAdmin subclasses

--- kasse/admin.py
+++ kasse/admin.py
@@ -4,6 +4,23 @@ from django.contrib import admin
 from kasse.models import Association, Title, Profile
 
 
+class TitleAdmin(admin.ModelAdmin):
+    list_select_related = ('association', 'profile')
+    list_display = (
+        '__str__', 'association', 'profile',
+    )
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        '__str__', 'is_anonymous',
+    )
+
+    def is_anonymous(self, o):
+        return o.is_anonymous
+    is_anonymous.boolean = True
+
+
 admin.site.register(Association)
-admin.site.register(Title)
-admin.site.register(Profile)
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Profile, ProfileAdmin)
