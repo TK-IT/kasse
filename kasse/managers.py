e8e6d75606b2f657c785f3db4687e33e987ee60d
Mathias Rav <rav@cs.au.dk>
2015-10-19 11:33:55 +0200

    Add display_name annotation (slightly buggy)

--- kasse/managers.py
+++ kasse/managers.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, unicode_literals, division
 
 from django.db import models
+from django.db.models import CharField, Case, Value, When, F
+from django.db.models.functions import Concat
 
 
 class ProfileManager(models.Manager):
@@ -9,4 +11,12 @@ class ProfileManager(models.Manager):
 
     def get_queryset(self):
         qs = super(ProfileManager, self).get_queryset()
+        qs = qs.annotate(
+            display_name=Case(
+                When(title__isnull=True, name='',
+                     then=Concat(Value('(anonymous '), F('id'), Value(')'))),
+                When(title__isnull=True, then='name'),
+                When(name='', then='title__title'),
+                default=Concat(F('title__title'), Value(' '), F('name')),
+                output_field=CharField()))
         return qs.select_related('association', 'title__association')
