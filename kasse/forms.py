7004865aa6dbf42b29d2c588102db32b6f12c374
Mathias Rav <rav@cs.au.dk>
2015-12-22 09:32:07 +0100

    Fix clean_period

--- kasse/forms.py
+++ kasse/forms.py
@@ -110,6 +110,8 @@ class ProfileEditForm(forms.ModelForm):
             raise ValidationError("Cleaned fields in the wrong order")
         p = self.cleaned_data['period']
         p = self.fields['period'].clean(p)
+        if p is None:
+            return p
         if not isinstance(p, six.integer_types):
             raise ValidationError("Period is %r, not an int; field is %s" %
                 (p, type(self.fields['period'])))
