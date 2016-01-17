57f91268edaf811bdc137badca6b4014c4b9aff2
Mathias Rav <rav@cs.au.dk>
2016-01-17 14:25:54 +0100

    Add news models to admin.py

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, unicode_literals, division
 
 from django.contrib import admin
-from news.models import Config
+from news.models import NewsProfile, Config, Post, Comment
 
 
+admin.site.register(NewsProfile, admin.ModelAdmin)
 admin.site.register(Config, admin.ModelAdmin)
+admin.site.register(Post, admin.ModelAdmin)
+admin.site.register(Comment, admin.ModelAdmin)
