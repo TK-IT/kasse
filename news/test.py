e36ce5e65a1d6454fcf16b67537300b6fd970be2
Mathias Rav <rav@cs.au.dk>
2016-01-23 15:52:33 +0100

    Bump date in news.test

--- news/test.py
+++ news/test.py
@@ -116,7 +116,7 @@ def main():
             sys.stdout.flush()
 
     created_times = TimeTrial.objects.filter(
-        created_time__gte=datetime.date(2016, 1, 16))
+        created_time__gte=datetime.date(2016, 1, 19))
     created_times = created_times.values_list('created_time', flat=True)
     created_times = sorted(created_times)
     created_times = [
