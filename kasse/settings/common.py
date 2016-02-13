f2715ce7afe623df3f7df002ee4208dbe968fa7b
Mathias Rav <rav@cs.au.dk>
2016-02-13 15:35:28 +0100

    Display images using sorl.thumbnail

--- kasse/settings/common.py
+++ kasse/settings/common.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
     'kasse.apps.KasseConfig',
     'stopwatch.apps.StopwatchConfig',
     'iou.apps.IouConfig',
