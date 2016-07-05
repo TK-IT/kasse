6a89635e9dfcfc831cf820585cd8c8fd8fbd34b6
Mathias Rav <rav@cs.au.dk>
2016-07-05 16:31:09 +0200

    Filter out all invalid hosts logs

--- kasse/log.py
+++ kasse/log.py
@@ -11,13 +11,4 @@ class AllowedHosts(logging.Filter):
     def filter(self, record):
         if record.name != 'django.security.DisallowedHost':
             return True
-        msg_pattern = r"^Invalid HTTP_HOST header: '(\S+)'\. .*"
-        mo = re.match(msg_pattern, record.msg)
-        if mo is None:
-            logger.info("Couldn't parse %r", record.msg, extra=dict(ip=None))
-            return True
-        host = mo.group(1)
-        ip_pattern = r'^\d+\.\d+\.\d+\.\d+$'
-        if re.match(ip_pattern, host):
-            return False
-        return True
+        return False
