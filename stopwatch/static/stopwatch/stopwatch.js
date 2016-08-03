384884f4d39f30b70721beb391ce0ce35fe5234f
Mathias Rav <rav@cs.au.dk>
2016-08-03 16:07:23 +0200

    Use iterators

--- stopwatch/static/stopwatch/stopwatch.js
+++ stopwatch/static/stopwatch/stopwatch.js
@@ -202,9 +202,30 @@ function reset(ev) {
     add_possible_lap("Reset");
     start_time = null;
     laps = [];
-    for (var i = 0; i < possible_laps.length; i += 1) {
-        possible_laps[i].lap = false;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = possible_laps[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var l = _step.value;
+            l.lap = false;
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
     }
+
     update_laps();
     div_stopwatch.className = 'initial';
     update_div_time(0, 2);
@@ -227,19 +248,34 @@ function window_click(ev) {
 
 // getCookie from https://docs.djangoproject.com/en/1.4/ref/contrib/csrf/
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+    var cookies = (document.cookie || '').split(';').map(String.trim);
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+        for (var _iterator2 = cookies[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var cookie = _step2.value;
+
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == name + '=') {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
+            if (cookie.substring(0, name.length + 1) === name + '=') return decodeURIComponent(cookie.substring(name.length + 1));
+        }
+    } catch (err) {
+        _didIteratorError2 = true;
+        _iteratorError2 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+                _iterator2.return();
+            }
+        } finally {
+            if (_didIteratorError2) {
+                throw _iteratorError2;
             }
         }
     }
-    return cookieValue;
+
+    return null;
 }
 var csrftoken = getCookie('csrftoken');
 
@@ -292,11 +328,32 @@ function update_state(state) {
 
     laps = [];
     var p = 0;
-    for (var i = 0; i < state['durations'].length; ++i) {
-        var l = 1000 * state['durations'][i] | 0;
-        p += l;
-        laps.push(p);
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
+
+    try {
+        for (var _iterator3 = state['durations'][Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+            var duration = _step3.value;
+
+            p += 1000 * duration | 0;
+            laps.push(p);
+        }
+    } catch (err) {
+        _didIteratorError3 = true;
+        _iteratorError3 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion3 && _iterator3.return) {
+                _iterator3.return();
+            }
+        } finally {
+            if (_didIteratorError3) {
+                throw _iteratorError3;
+            }
+        }
     }
+
     update_laps();
 
     var button_label = 'Live';
@@ -346,13 +403,33 @@ function takePictureChange(ev) {
     var files = ev.target.files || [];
     if (files.length === 0) console.log("files is empty");
     div_pictures.innerHTML = '';
-    for (var i = 0; i < files.length; ++i) {
-        var file = files[i];
-        var imgURL = URL.createObjectURL(file);
-        var img = document.createElement('img');
-        img.src = imgURL;
-        URL.revokeObjectURL(imgURL);
-        div_pictures.appendChild(img);
+    var _iteratorNormalCompletion4 = true;
+    var _didIteratorError4 = false;
+    var _iteratorError4 = undefined;
+
+    try {
+        for (var _iterator4 = files[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+            var file = _step4.value;
+
+            var imgURL = URL.createObjectURL(file);
+            var img = document.createElement('img');
+            img.src = imgURL;
+            URL.revokeObjectURL(imgURL);
+            div_pictures.appendChild(img);
+        }
+    } catch (err) {
+        _didIteratorError4 = true;
+        _iteratorError4 = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion4 && _iterator4.return) {
+                _iterator4.return();
+            }
+        } finally {
+            if (_didIteratorError4) {
+                throw _iteratorError4;
+            }
+        }
     }
 }
 
