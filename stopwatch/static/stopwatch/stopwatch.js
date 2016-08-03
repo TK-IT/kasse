7b5861f0dc287de07f7fb002b2f29653477788ba
Mathias Rav <rav@cs.au.dk>
2016-08-03 16:01:42 +0200

    Use let and const

--- stopwatch/static/stopwatch/stopwatch.js
+++ stopwatch/static/stopwatch/stopwatch.js
@@ -1,4 +1,6 @@
-// vim:set sw=4 sts=4 ts=4 et:
+'use strict';
+
+// vim:set ft=javascript sw=4 sts=4 ts=4 et:
 var start_time = null;
 var stopped = true;
 var laps = [];
@@ -15,29 +17,21 @@ var fetch_interval = null;
 var ta_current = null;
 
 function format_difference(total_milliseconds, n) {
-    var s = (total_milliseconds > 0) ? '+' : '&minus;';
+    var s = total_milliseconds > 0 ? '+' : '&minus;';
     if (total_milliseconds < 0) total_milliseconds = -total_milliseconds;
-    var seconds = (total_milliseconds / 1000)|0;
-    var milliseconds = (total_milliseconds - 1000 * seconds)|0;
-    return (
-        s + seconds + '.' +
-        ('000' + milliseconds).slice(-3, -3 + n)
-    );
+    var seconds = total_milliseconds / 1000 | 0;
+    var milliseconds = total_milliseconds - 1000 * seconds | 0;
+    return s + seconds + '.' + ('000' + milliseconds).slice(-3, -3 + n);
 }
 
 function format_timestamp(total_milliseconds, n) {
-    var total_seconds = (total_milliseconds / 1000)|0;
-    var milliseconds = (total_milliseconds - 1000 * total_seconds)|0;
-    var total_minutes = (total_seconds / 60)|0;
-    var seconds = (total_seconds - 60 * total_minutes)|0;
-    var total_hours = (total_minutes / 60)|0;
+    var total_seconds = total_milliseconds / 1000 | 0;
+    var milliseconds = total_milliseconds - 1000 * total_seconds | 0;
+    var total_minutes = total_seconds / 60 | 0;
+    var seconds = total_seconds - 60 * total_minutes | 0;
+    var total_hours = total_minutes / 60 | 0;
     var minutes = total_minutes - 60 * total_hours;
-    return (
-        total_hours + ':' +
-        ('00' + minutes).slice(-2) + ':' +
-        ('00' + seconds).slice(-2) + '.' +
-        ('000' + milliseconds).slice(-3, -3 + n)
-    );
+    return total_hours + ':' + ('00' + minutes).slice(-2) + ':' + ('00' + seconds).slice(-2) + '.' + ('000' + milliseconds).slice(-3, -3 + n);
 }
 
 function update_div_time(total_milliseconds, n) {
@@ -49,7 +43,7 @@ function update_div_time(total_milliseconds, n) {
 function update_time() {
     if (!stopped) {
         var now = new Date().getTime();
-        var total_milliseconds = (now - start_time)|0;
+        var total_milliseconds = now - start_time | 0;
         update_div_time(total_milliseconds, 1);
         window.requestAnimationFrame(update_time);
     }
@@ -58,17 +52,12 @@ function update_time() {
 function lap_element(index, duration, total, difference) {
     var o = document.createElement('div');
     o.className = 'lap';
-    var h = (
-        '<div class="lapIndex">Øl ' + index + '</div>' +
-        '<div class="lapDuration">' + format_timestamp(duration, 2) + '</div>' +
-        '<div class="lapTotal">' + format_timestamp(total, 2) + '</div>'
-    );
+    var h = '<div class="lapIndex">Øl ' + index + '</div>' + '<div class="lapDuration">' + format_timestamp(duration, 2) + '</div>' + '<div class="lapTotal">' + format_timestamp(total, 2) + '</div>';
     if (difference !== null) {
-        var c = (difference <= 0) ? "negdiff" : "posdiff";
-        h += ('<div class="lapDiff ' + c + '">' +
-              format_difference(difference, 2) + '</div>');
+        var c = difference <= 0 ? "negdiff" : "posdiff";
+        h += '<div class="lapDiff ' + c + '">' + format_difference(difference, 2) + '</div>';
     } else {
-        h += ('<div class="lapDiff"></div>');
+        h += '<div class="lapDiff"></div>';
     }
     o.innerHTML = h;
     return o;
@@ -80,7 +69,7 @@ function update_laps() {
     var ta_cumsum = 0;
     var v = [];
     for (var i = 0; i < laps.length; ++i) {
-        var duration = (laps[i] - prev)|0;
+        var duration = laps[i] - prev | 0;
         prev = laps[i];
         v.push(duration / 1000);
         if (time_attack) {
@@ -89,24 +78,20 @@ function update_laps() {
         var difference = null;
         if (time_attack) {
             if (time_attack.durations.length > i) {
-                difference = (laps[i] - ta_cumsum) | 0;
+                difference = laps[i] - ta_cumsum | 0;
             } else {
                 difference = null;
             }
         }
-        div_laps.appendChild(
-            lap_element(i + 1, duration, laps[i], difference));
+        div_laps.appendChild(lap_element(i + 1, duration, laps[i], difference));
     }
-    div_laps.className = (laps.length > 8) ? 'many' : '';
+    div_laps.className = laps.length > 8 ? 'many' : '';
 
     ta_current = null;
     var ta_len = time_attack ? time_attack.durations.length : 0;
     if (time_attack && ta_len > laps.length) {
         ta_cumsum += time_attack.durations[laps.length];
-        var h = (
-            '<div class="lapIndex"></div><div class="lapDuration"></div>' +
-            '<div class="lapTotal"></div><div class="lapDiff" ' +
-            ' id="ta_current"></div>');
+        var h = '<div class="lapIndex"></div><div class="lapDuration"></div>' + '<div class="lapTotal"></div><div class="lapDiff" ' + ' id="ta_current"></div>';
         var o = document.createElement('div');
         o.className = 'lap';
         o.innerHTML = h;
@@ -115,7 +100,7 @@ function update_laps() {
     }
 
     if (btn_lap !== null) {
-        btn_lap.textContent = 'Færdig med øl '+(1 + laps.length);
+        btn_lap.textContent = 'Færdig med øl ' + (1 + laps.length);
     }
     if (btn_continue !== null) {
         btn_continue.textContent = 'Fortsæt med øl ' + (1 + laps.length);
@@ -131,9 +116,10 @@ function update_ta_current(now, n) {
     ta_current.style.display = stopped ? 'none' : '';
     var ta_cumsum = 0;
     var l = laps.length + 1;
-    for (var i = 0; i < l; ++i) ta_cumsum += time_attack.durations[i]|0;
-    var d = now - ta_cumsum;
-    var c = (d <= 0) ? "negdiff" : "posdiff";
+    for (var i = 0; i < l; ++i) {
+        ta_cumsum += time_attack.durations[i] | 0;
+    }var d = now - ta_cumsum;
+    var c = d <= 0 ? "negdiff" : "posdiff";
     ta_current.className = 'lapDiff ' + c;
     ta_current.innerHTML = format_difference(d, n);
 }
@@ -155,20 +141,20 @@ function lap(ev) {
     ev.preventDefault();
     ev.stopPropagation();
     var n = new Date().getTime();
-    try_add_lap((n - start_time)|0);
+    try_add_lap(n - start_time | 0);
 }
 
 function lap_touchstart(ev) {
     ev.preventDefault();
     ev.stopPropagation();
     var n = new Date().getTime();
-    try_add_lap((n - start_time)|0);
+    try_add_lap(n - start_time | 0);
 }
 
 function add_possible_lap(comment) {
     if (start_time === null) return;
     var n = new Date().getTime();
-    var d = (n - start_time) | 0;
+    var d = n - start_time | 0;
     possible_laps.push({
         'time': d,
         'lap': false,
@@ -179,7 +165,7 @@ function add_possible_lap(comment) {
 function try_add_lap(d) {
     var min_length = 1000;
     if (laps.length === 0) min_length = 3000;
-    var prev_lap = (laps.length === 0) ? 0 : laps[laps.length - 1];
+    var prev_lap = laps.length === 0 ? 0 : laps[laps.length - 1];
     if (d - prev_lap < min_length) {
         possible_laps.push({
             'time': d,
@@ -190,7 +176,7 @@ function try_add_lap(d) {
         possible_laps.push({
             'time': d,
             'lap': true,
-            'comment': "Tilføjet som Øl " + (1 + laps.length),
+            'comment': "Tilføjet som Øl " + (1 + laps.length)
         });
         laps.push(d);
         update_laps();
@@ -247,7 +233,7 @@ function getCookie(name) {
         for (var i = 0; i < cookies.length; i++) {
             var cookie = jQuery.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+            if (cookie.substring(0, name.length + 1) == name + '=') {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
             }
@@ -261,11 +247,11 @@ function post_live_update() {
     if (post_pk === null) return;
     var url = reverse('timetrial_liveupdate', post_pk);
     var now = new Date().getTime();
-    var n;
+    var n = undefined;
     if (start_time === null) {
         n = 0;
     } else {
-        n = (now - start_time)|0;
+        n = now - start_time | 0;
     }
     var data = {
         'csrfmiddlewaretoken': csrftoken,
@@ -277,8 +263,8 @@ function post_live_update() {
         'state': div_stopwatch.className
     };
     function measure_roundtrip() {
-        roundtrip_estimate = (new Date().getTime() - now)|0;
-        console.log("roundtrip_estimate: "+roundtrip_estimate+" ms");
+        roundtrip_estimate = new Date().getTime() - now | 0;
+        console.log("roundtrip_estimate: " + roundtrip_estimate + " ms");
     };
     $.post(url, data).always(measure_roundtrip);
 }
@@ -286,30 +272,28 @@ function post_live_update() {
 function fetch_state() {
     var now = new Date().getTime();
     function success(data) {
-        roundtrip_estimate = (new Date().getTime() - now)|0;
-        console.log("roundtrip_estimate: "+roundtrip_estimate+" ms");
-        data['elapsed_time'] = (
-            data['elapsed_time'] + roundtrip_estimate / 2000);
+        roundtrip_estimate = new Date().getTime() - now | 0;
+        console.log("roundtrip_estimate: " + roundtrip_estimate + " ms");
+        data['elapsed_time'] = data['elapsed_time'] + roundtrip_estimate / 2000;
         console.log(data);
         update_state(data);
     }
     function fail(jqxhr, textStatus, error) {
         var btn = document.getElementById('live');
         if (btn) btn.textContent = 'Fejl';
-        document.getElementById('stopwatchlog').appendChild(
-            document.createTextNode(textStatus + ', ' + error + '\n'));
+        document.getElementById('stopwatchlog').appendChild(document.createTextNode(textStatus + ', ' + error + '\n'));
     }
     $.getJSON('.', success).fail(fail);
 }
 
 function update_state(state) {
-    var elapsed = (state['elapsed_time'] * 1000)|0;
+    var elapsed = state['elapsed_time'] * 1000 | 0;
     start_time = new Date().getTime() - elapsed;
 
     laps = [];
     var p = 0;
     for (var i = 0; i < state['durations'].length; ++i) {
-        var l = (1000 * state['durations'][i])|0;
+        var l = 1000 * state['durations'][i] | 0;
         p += l;
         laps.push(p);
     }
@@ -362,7 +346,7 @@ function takePictureChange(ev) {
     var files = ev.target.files || [];
     if (files.length === 0) console.log("files is empty");
     div_pictures.innerHTML = '';
-    for (var i = 0, l = files.length; i < l; ++i) {
+    for (var i = 0; i < files.length; ++i) {
         var file = files[i];
         var imgURL = URL.createObjectURL(file);
         var img = document.createElement('img');
@@ -409,7 +393,7 @@ function init() {
     var takePicture = document.getElementById('take-picture');
     if (takePicture !== null) {
         takePicture.addEventListener('change', takePictureChange, false);
-        takePictureChange({target: takePicture});
+        takePictureChange({ target: takePicture });
     }
 }
 
