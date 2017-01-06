b8995d573caf4b38aa31207485887dc9eea5c8b5
Martey Dodoo <martey@mobolic.com>
2017-01-05 19:40:20 -0500

    Change request method's args default again.
    Fix error caused by mobolic/facebook-sdk#340 which could cause args' default value to be a
    dict containing arguments from past uses of the request method. Thanks
    to @fractalis for reporting this.
    
    Upstream-ref: https://github.com/mobolic/facebook-sdk/commit/bd40c39dc987a0c490847afe867f5bae05df97a9

--- facebook_api.py
+++ facebook_api.py
@@ -225,7 +225,7 @@ class GraphAPI(object):
         except Exception:
             raise GraphAPIError("API version number not available")
 
-    def request(self, path, args=dict(), post_args=None, files=None, method=None):
+    def request(self, path, args=None, post_args=None, files=None, method=None):
         """Fetches the given path in the Graph API.
 
         We translate args to a valid query string. If post_args is
@@ -233,7 +233,8 @@ class GraphAPI(object):
         arguments.
 
         """
-
+        if args is None:
+            args = dict()
         if post_args is not None:
             method = "POST"
 
