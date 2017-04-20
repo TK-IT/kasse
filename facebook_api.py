47d5a443bd191df1d38af8ccaab812c520388941
19spdev <19spdev@gmail.com>
2017-04-20 06:53:34 -0300

    Add search method.
    Upstream-ref: https://github.com/mobolic/facebook-sdk/commit/0156b69366fc99930dd81edafa62c5cc600e90bd

--- facebook_api.py
+++ facebook_api.py
@@ -41,6 +41,7 @@ except ImportError:
 
 FACEBOOK_GRAPH_URL = "https://graph.facebook.com/"
 FACEBOOK_OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?"
+VALID_SEARCH_TYPES = ["page", "event", "group", "place", "placetopic", "user"]
 
 
 class GraphAPI(object):
@@ -112,6 +113,18 @@ class GraphAPI(object):
         """
         args["ids"] = ",".join(ids)
         return self.request(self.version + "/", args)
+
+    def search(self, type, **args):
+        """Fetches all objects of a given type from the graph.
+
+        Returns all objects of a given type from the graph as a dict.
+        """
+
+        if type not in VALID_SEARCH_TYPES:
+            raise GraphAPIError("Valid types are: %s" % ", ".join(VALID_SEARCH_TYPES))
+
+        args["type"] = type
+        return self.request(self.version + "/search/", args)
 
     def get_connections(self, id, connection_name, **args):
         """Fetches the connections for given object."""
