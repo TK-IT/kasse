56c8b19a43b7b9e0ce72eae2047066ccefe7c942
Mathias Rav <m@git.strova.dk>
2018-01-03 13:23:47 +0100

    Update get_season_start

--- kasse/views.py
+++ kasse/views.py
@@ -85,7 +85,7 @@ class Home(TemplateView):
 
     @staticmethod
     def get_season_start():
-        return datetime.date(2016, 8, 1)
+        return datetime.date(2017, 9, 18)
 
     def get_current_best(self, **kwargs):
         season_start = Home.get_season_start()
